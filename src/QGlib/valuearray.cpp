8b13f290525edde8c904b1a00f66b671151e9444
Diane Trout <diane@ghic.org>
2014-05-29 22:07:37 -0700

    Implement the rest of ValueArray (especially the iterator)

--- src/QGlib/valuearray.cpp
+++ src/QGlib/valuearray.cpp
@@ -1,8 +1,32 @@
 #include "valuearray.h"
+
+#define GLIB_DISABLE_DEPRECATION_WARNINGS
+
 #include <glib-object.h>
 #include <qmutex.h>
 
 namespace QGlib {
+
+const Value &ValueArray::const_iterator::operator[](int j)
+{
+    Value temp(g_value_array_get_nth(const_cast<GValueArray *>(a->garray), i + j));
+    cache = temp;
+    return cache;
+}
+
+const Value &ValueArray::const_iterator::operator*()
+{
+    Value temp(g_value_array_get_nth(const_cast<GValueArray *>(a->garray), i));
+    cache = temp;
+    return cache;
+}
+
+const Value *ValueArray::const_iterator::operator->()
+{
+    Value temp(g_value_array_get_nth(const_cast<GValueArray *>(a->garray), i));
+    cache = temp;
+    return &cache;
+}
 
 ValueArray::ValueArray() :
   garray(0)
@@ -11,6 +35,11 @@ ValueArray::ValueArray() :
 
 ValueArray::ValueArray(QGlib::Value &value) :
   garray(static_cast<GValueArray *>(g_value_get_boxed(value)))
+{
+}
+
+ValueArray::ValueArray(int size) :
+  garray(g_value_array_new(size))
 {
 }
 
@@ -59,6 +88,11 @@ const QGlib::Value ValueArray::back() const
     return at( size() - 1 );
 }
 
+ValueArray::const_iterator ValueArray::begin() const
+{
+    return const_iterator(this);
+}
+
 void ValueArray::clear()
 {
     if (garray) {
@@ -67,15 +101,70 @@ void ValueArray::clear()
     }
 }
 
+ValueArray::const_iterator ValueArray::constBegin() const
+{
+    return begin();
+}
+
+ValueArray::const_iterator ValueArray::constEnd() const
+{
+    return end();
+}
+
+int ValueArray::count() const
+{
+    return size();
+}
+
+bool ValueArray::empty() const
+{
+    return isEmpty();
+}
+
+ValueArray::const_iterator ValueArray::end() const
+{
+    return const_iterator(this, size());
+}
+
+const Value ValueArray::first() const
+{
+    return at(0);
+}
+
+const Value ValueArray::front() const
+{
+    return first();
+}
+
 void ValueArray::insert( int i, const QGlib::Value &value)
 {
     Q_ASSERT_X(garray, "ValueArray::insert", "Called with uninitialized array");
     garray = g_value_array_insert(garray, i, value);
 }
 
+bool ValueArray::isEmpty() const
+{
+    return size() == 0;
+}
+
+const Value ValueArray::last() const
+{
+    return at(size() - 1);
+}
+
 int ValueArray::length() const
 {
     return size();
+}
+
+void ValueArray::pop_back()
+{
+    removeLast();
+}
+
+void ValueArray::pop_front()
+{
+    removeFirst();
 }
 
 void ValueArray::prepend(const QGlib::Value& value)
@@ -101,6 +190,24 @@ int ValueArray::removeAt(int i)
     garray = g_value_array_remove(garray, i);
 }
 
+void ValueArray::removeFirst()
+{
+    removeAt(0);
+}
+
+void ValueArray::removeLast()
+{
+    removeAt(size() - 1);
+}
+
+void ValueArray::replace(int i, QGlib::Value &source)
+{
+    Q_ASSERT_X(garray, "ValueArray::set", "Called with uninitialized array");
+
+    GValue *destination = g_value_array_get_nth(garray, i);
+    g_value_copy(static_cast<GValue *>(source), destination);
+}
+
 int ValueArray::size() const
 {
     if (garray) {
@@ -110,17 +217,38 @@ int ValueArray::size() const
     }
 }
 
+void ValueArray::swap(int i, int j)
+{
+    GValue *a = g_value_array_get_nth(garray, i);
+    GValue *b = g_value_array_get_nth(garray, j);
+    GValue temp = G_VALUE_INIT;
+    g_value_init(&temp, G_VALUE_TYPE(a));
+
+    g_value_copy(a, &temp);
+    g_value_copy(b, a);
+    g_value_copy(&temp, b);
+}
+
+Value ValueArray::takeAt(int i)
+{
+    Value rv(at(i));
+    removeAt(i);
+    return rv;
+}
+
+Value ValueArray::takeFirst()
+{
+    return takeAt(0);
+}
+
+Value ValueArray::takeLast()
+{
+    return takeAt(size() - 1);
+}
+
 const QGlib::Value ValueArray::operator[](int i) const
 {
     return at(i);
-}
-
-void ValueArray::replace(int i, QGlib::Value &source)
-{
-    Q_ASSERT_X(garray, "ValueArray::set", "Called with uninitialized array");
-
-    GValue *destination = g_value_array_get_nth(garray, i);
-    g_value_copy(static_cast<GValue *>(source), destination);
 }
 
 QList< QGlib::Value > ValueArray::toList() const
@@ -180,5 +308,15 @@ ValueArray &ValueArray::operator=(GValueArray *array)
     garray = array;
     return *this;
 }
+
+// Implementing these functions need a working equality test for QGlib::Values
+// bool ValueArray::contains(const Value &value) const
+// int ValueArray::count(const Value &value) const
+// bool ValueArray::endsWith(const Value &value) const
+// int ValueArray::indexOf(const Value &value, int from) const
+// int ValueArray::lastIndexOf(const Value &value, int from) const
+// int ValueArray::removeAll(const Value &value)
+// bool ValueArray::removeOne(const Value &value)
+// bool ValueArray::startsWith(const Value &value) const
 
 } //namespace QGlib
--- src/QGlib/valuearray.h
+++ src/QGlib/valuearray.h
@@ -26,53 +26,123 @@ namespace QGlib {
  * \brief Wrapper for GValueArray
  *
  * This is a proxy class to enable interacting with
- * GValueArrays
+ * GValueArrays it is largely inspired by QList/QVector
  *
- * Unfortunately since this class constructs QGlib:Values to
- * return them, it can't return a reference.
- * And because they're new Values each time there's no easy way to modify them.
+ * Unfortunately since this class constructs new QGlib:Values
+ * intead of returning references which makes it impossilbe
+ * to easily update elements of the array.
+ *
+ * If you do need to change a value you can use the replace
+ * function.
+ *
+ * Additionally I only implemented a const_iterator.
+ * Because iterators really need to return a pointer to an
+ * existing object the iterator maintains a cached copy of
+ * the value its currently looking at. This means the iterator
+ * is fairly slow, and is absolutely not thread-safe.
  */
 
 class QTGLIB_EXPORT ValueArray
 {
 public:
+
+    class const_iterator {
+        const ValueArray *a;
+        int i;
+        Value cache;
+
+    public:
+        typedef Value value_type;
+        typedef Value *pointer;
+        typedef Value &reference;
+
+        inline const_iterator() : a(0), i(0) {}
+        inline const_iterator(const ValueArray *v, int j=0) : a(v), i(j) {}
+        inline const_iterator(const const_iterator &o) : a(o.a), i(o.i) {}
+        const Value &operator[](int j);
+        const Value &operator*();
+        const Value *operator->();
+        inline bool operator==(const const_iterator &o) const { return a == o.a && i == o.i; }
+        inline bool operator!=(const const_iterator &o) const { return a != o.a || i != o.i; }
+        inline bool operator<(const const_iterator &o) const { return i < o.i; }
+        inline bool operator<=(const const_iterator &o) const { return i <= o.i; }
+        inline bool operator>(const const_iterator &o) const { return i > o.i; }
+        inline bool operator>=(const const_iterator &o) const { return i >= o.i; }
+        inline const_iterator &operator++() { ++i; return *this; }
+        inline const_iterator &operator--() { --i; return *this; }
+        inline const_iterator &operator+=(int j) { i += j; return *this; }
+        inline const_iterator &operator-=(int j) { i -= j; return *this; }
+
+        friend class ValueArray;
+    };
+    friend class const_iterator;
+
     ValueArray();
     ValueArray(Value &value);
+    ValueArray(int);
     explicit ValueArray(GValueArray *);
-    ValueArray(QList< Value> &iterable);
-    ValueArray(QVector< Value> &iterable);
+    ValueArray(QList< Value > &iterable);
+    ValueArray(QVector< Value > &iterable);
 
     ~ValueArray();
 
     void append (const Value &value);
     const Value at(int i) const;
     const Value back() const;
+    //iterator begin();
+    const_iterator begin() const;
     void clear();
-
+    const_iterator constBegin() const;
+    const_iterator constEnd() const;
+    // bool contains(const Value &value) const;
+    // int count(const Value &value) const;
+    int count() const;
+    bool empty() const;
+    //iterator end();
+    const_iterator end() const;
+    // bool endsWith(const Value &) const;
+    // iterator erase(iterator pos);
+    // iterator erase(iterator begin, iterator end);
+    const Value first() const;
+    const Value front() const;
+    // int indexOf(const Value &value, int from=0) const;
     void insert( int i, const Value &value);
+    bool isEmpty() const;
+    const Value last() const;
+    // int lastIndexOf(const Value &value, int from=-1) const;
     int length() const;
-
+    // mid?
+    void move(int from, int to);
+    void pop_back();
+    void pop_front();
     void prepend(const Value& value);
     void push_back(const Value &value);
     void push_front(const Value &value);
-
+    // int removeAll(const Value &value);
     int removeAt(int i);
+    void removeFirst();
+    void removeLast();
+    // bool removeOne(const Value &value);
     void replace(int, Value &);
-
+    //void reserve(int alloc);
     int size() const;
-    // Value operator [](int);
-    const Value operator[](int i) const;
-
-
+    // bool startsWith(const Value &value) const;
+    void swap(int i, int j);
+    Value takeAt(int i);
+    Value takeFirst();
+    Value takeLast();
     QList< Value > toList() const;
+    std::list< Value > toStdList() const;
     QVector< Value > toVector() const;
-
+    std::vector< Value > toStdVector() const;
     Value value(int) const;
     Value value(int, const Value &) const;
 
-    operator GValueArray *();
-    ValueArray & operator=(const ValueArray & other);
+    ValueArray &operator=(const ValueArray & other);
     ValueArray &operator=(GValueArray *array);
+    // Value operator [](int);
+    const Value operator[](int i) const;
+    operator GValueArray *();
 
 protected:
     GValueArray *garray;
