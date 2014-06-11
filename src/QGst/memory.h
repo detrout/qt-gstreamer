ed35f2a29df2decd81af053c04f6311a5214d24f
Diane Trout <diane@ghic.org>
2014-06-11 08:31:30 -0700

    Add copyright block to allocator.cpp, update date in .h and memory.*

--- src/QGst/memory.h
+++ src/QGst/memory.h
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2013  Diane Trout <diane@ghic.org>
+    Copyright (C) 2014  Diane Trout <diane@ghic.org>
 
     This library is free software; you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published
